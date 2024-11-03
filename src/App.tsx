3dd063b42a26edad82f929b166507e9ae9215552
josuemj <98498772+josuemj@users.noreply.github.com>
2024-11-02 21:56:29 -0600

    feat: sketching front end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
+      <div className="appContainer">
+        <h1>ToDo</h1>
+
+        <div className="inputContainer">
+          <input className="addTaskInput" placeholder="Add Task"></input>
+          <button className="addTaskButton">
+            <h1>+</h1>
+          </button>
+        </div>
+
+        <div className="tasksContainer">
+          <div className="taskContainer"></div>
+          <div className="taskContainer">Completed</div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

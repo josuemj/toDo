f9f784a58df1d02fa8d5b9aa7ba48331fb7e95d1
josuemj <98498772+josuemj@users.noreply.github.com>
2024-11-02 22:08:29 -0600

    feat -> simply adding items to pending list

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
+import { SetStateAction, useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputTask, setInputTask] = useState<string>("");
+  const [pendingList, setPendingList] = useState<string[]>([]);
+
+  const handleInputChange = (event: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setInputTask(event.target.value);
+  };
+
+  const onAddTask = () => {
+    setPendingList([...pendingList, inputTask])
+  };
+
   return (
     <>
       <div className="appContainer">
         <h1>ToDo</h1>
 
         <div className="inputContainer">
-          <input className="addTaskInput" placeholder="Add Task"></input>
-          <button className="addTaskButton">
+          <input
+            className="addTaskInput"
+            placeholder="Add Task"
+            onChange={handleInputChange}
+          ></input>
+          <button className="addTaskButton" onClick={onAddTask}>
             <h1>+</h1>
           </button>
         </div>
 
         <div className="tasksContainer">
-          <div className="taskContainer"></div>
+          <div className="taskContainer">
+            pending
+
+            {pendingList.map( (task, index) => (
+              <li key={index}>{task}</li>
+            ))}
+          </div>
           <div className="taskContainer">Completed</div>
         </div>
       </div>
